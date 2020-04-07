22b39e7a3fb472ccb842ec4b6d4910fb91f55a09
Kevin Feng <kjfeng@princeton.edu>
2020-04-06 20:55:33 -0700

    drawer can go up

--- Assets/Scripts/DrawerInteractions.cs
+++ Assets/Scripts/DrawerInteractions.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DrawerInteractions : MonoBehaviour
 {
   private GameObject[] drawerElements;
-  private float distanceToTop = (float) (105.0 + 277.1);
+  private float cHeight;
+  private float buttonHeight;
+  private float distanceToTop = (float) 300.0;
   private GameObject upArrow;
   private GameObject downArrow;
 
@@ -13,7 +16,22 @@ public class DrawerInteractions : MonoBehaviour
     void Start()
     {
       upArrow = GameObject.Find("dButton").transform.Find("up-arrow").gameObject;
+      upArrow.GetComponent<Text>().text = "^";
+
       downArrow = GameObject.Find("dButton").transform.Find("down-arrow").gameObject;
+      downArrow.GetComponent<Text>().text = "";
+
+      GameObject canvas = GameObject.Find("Canvas");
+      cHeight = canvas.GetComponent<Canvas>().pixelRect.height;
+
+      buttonHeight = GameObject.Find("dButton").GetComponent<RectTransform>().rect.height;
+
+      RectTransform r = GameObject.Find("Drawer").GetComponent<RectTransform>();
+      r.position = new Vector3(r.position.x, -r.position.y + buttonHeight, r.position.z);
+
+      distanceToTop = cHeight - buttonHeight;
+
+      Debug.Log(cHeight);
     }
 
     // moving the drawer up + switching the arrows
@@ -24,8 +42,8 @@ public class DrawerInteractions : MonoBehaviour
         RectTransform rtf = go.GetComponent<RectTransform>();
         rtf.position = new Vector3(rtf.position.x, rtf.position.y + distanceToTop, rtf.position.z);
       }
-      upArrow.GetComponent<Renderer>().enabled = false;
-      downArrow.GetComponent<Renderer>().enabled = true;
+      upArrow.GetComponent<Text>().text = "";
+      downArrow.GetComponent<Text>().text = "^";
     }
 
     // moving the drawer down + switching the arrows
@@ -34,9 +52,9 @@ public class DrawerInteractions : MonoBehaviour
       foreach (GameObject go in drawerElements) {
         RectTransform rtf = go.GetComponent<RectTransform>();
         rtf.position = new Vector3(rtf.position.x, rtf.position.y - distanceToTop, rtf.position.z);
-        upArrow.GetComponent<Renderer>().enabled = true;
-        downArrow.GetComponent<Renderer>().enabled = false;
       }
+      upArrow.GetComponent<Text>().text = "^";
+      downArrow.GetComponent<Text>().text = "";
     }
 
 
