4df05a32b54b7d97930b85ffb79d8c3ec9e7d989
Kevin Feng <kjfeng@princeton.edu>
2020-04-17 13:45:52 -0700

    code setup for highlight switching

---
+++ Assets/Scripts/HighlightTracker.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HighlightTracker : MonoBehaviour
+{
+  public Image lastHighlight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject highlight = GameObject.Find("Button").transform.GetChild(0).gameObject;
+        Image im = highlight.GetComponent<Image>();
+        im.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
