9552fac1b57fd26cc3d14dc9a419fc49558787ce
Kevin Feng <kjfeng@princeton.edu>
2020-04-18 21:17:44 -0700

    added scroller as drawer background

--- Assets/Scripts/ButtonScript.cs
+++ Assets/Scripts/ButtonScript.cs
@@ -5,8 +5,20 @@ using UnityEngine.UI;
 
 public class ButtonScript : MonoBehaviour
 {
+    public GameObject categoryScroll;
+    private int count;
 
-    public void showHighlight() {
+    // Start is called before the first frame update
+    void Start()
+    {
+      // categoryScroll = GameObject.Find("Category Scroll");
+      // GameObject.Find("Category Scroll").SetActive(false);
+      categoryScroll.SetActive(false);
+      count = 0;
+    }
+
+    public void changeButton() {
+      count++;
       GameObject thisHighlight = this.gameObject.transform.GetChild(0).gameObject;
       Image thisImage = thisHighlight.GetComponent<Image>();
       Image lastImg = HighlightTracker.lastHighlight;
@@ -15,13 +27,17 @@ public class ButtonScript : MonoBehaviour
         lastImg.enabled = false;
       }
       HighlightTracker.lastHighlight = thisImage;
+      if (categoryScroll == null) {
+        Debug.Log("nooo");
+      }
+      if (count % 2 != 0) {
+        categoryScroll.SetActive(true);
+      } else {
+        categoryScroll.SetActive(false);
+      }
 
     }
-    // Start is called before the first frame update
-    void Start()
-    {
 
-    }
 
     // Update is called once per frame
     void Update()
